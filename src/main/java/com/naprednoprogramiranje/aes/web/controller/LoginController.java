ff1c08f729682bc8d8a80274825a49c12b64157b
Katarina Vucic <vucic.kat@gmail.com>
2022-01-15 22:02:45 +0100

    Created login page

--- src/main/java/com/naprednoprogramiranje/aes/web/controller/LoginController.java
+++ src/main/java/com/naprednoprogramiranje/aes/web/controller/LoginController.java
@@ -1,20 +1,17 @@
 package com.naprednoprogramiranje.aes.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("")
-public class IndexController {
+public class LoginController {
 
-    @GetMapping({"/", "/index"})
-    public String index() {
-        return "website/index";
-    }
-
-    @GetMapping("/login")
-    public String login() {
+    @GetMapping("/login-error")
+    public String loginError(Model model) {
+        model.addAttribute("loginError", true);
         return "website/login";
     }
 
