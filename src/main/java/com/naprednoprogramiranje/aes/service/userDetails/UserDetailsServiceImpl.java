756ee054701d5326a9441fbd49101241600d55c9
Katarina Vucic <vucic.kat@gmail.com>
2022-01-15 21:56:43 +0100

    Configured DaoAuthenticationProvider and HttpSecurity

---
+++ src/main/java/com/naprednoprogramiranje/aes/service/userDetails/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.naprednoprogramiranje.aes.service.userDetails;
+
+import com.naprednoprogramiranje.aes.model.User;
+import com.naprednoprogramiranje.aes.service.user.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserService userService;
+
+    public UserDetailsServiceImpl(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.findByUsername(username);
+        if (user == null) {
+            log.error("User not found in the database");
+            throw new UsernameNotFoundException("User not found in the database");
+        } else {
+            log.info("User found in the database: {}", username);
+        }
+        return new UserDetailsImpl(user);
+    }
+}
