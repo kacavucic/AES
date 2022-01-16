ad6d94685cf7d7d38b2c35eb436a968842306c33
Katarina Vucic <vucic.kat@gmail.com>
2022-01-16 15:43:43 +0100

    Implemented verify document signing functionality

--- src/main/java/com/naprednoprogramiranje/aes/web/controller/UploadController.java
+++ src/main/java/com/naprednoprogramiranje/aes/web/controller/UploadController.java
@@ -1,8 +1,16 @@
 package com.naprednoprogramiranje.aes.web.controller;
 
+import com.google.common.hash.HashCode;
+import com.google.common.hash.Hashing;
+import com.google.common.io.Files;
+import com.maxmind.geoip2.exception.GeoIp2Exception;
 import com.naprednoprogramiranje.aes.model.SigningSession;
 import com.naprednoprogramiranje.aes.model.User;
 import com.naprednoprogramiranje.aes.service.email.EmailService;
+import com.naprednoprogramiranje.aes.service.ipaddress.GeoIP;
+import com.naprednoprogramiranje.aes.service.ipaddress.IPLocationService;
+import com.naprednoprogramiranje.aes.service.ipaddress.RequestService;
+import com.naprednoprogramiranje.aes.service.signing.SigningService;
 import com.naprednoprogramiranje.aes.service.signingSession.SigningSessionService;
 import com.naprednoprogramiranje.aes.service.storage.StorageService;
 import com.naprednoprogramiranje.aes.service.totp.OTC;
@@ -13,6 +21,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,7 +29,13 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.mail.MessagingException;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
+import java.security.GeneralSecurityException;
+import java.util.Optional;
 import java.util.UUID;
 
 @Controller
@@ -32,6 +47,8 @@ public class UploadController {
     private TotpService totpService;
     private StorageService storageService;
     private SigningSessionService signingSessionService;
+    private RequestService requestService;
+    private IPLocationService locationService;
 
     @PostMapping(value = "/upload-document")
     public ModelAndView uploadDocument(ModelAndView modelAndView, @RequestParam("document") MultipartFile document,
@@ -68,4 +85,56 @@ public class UploadController {
         return modelAndView;
     }
 
+    @PostMapping(value = "/verify-signing")
+    public ModelAndView verifySigning(ModelAndView modelAndView,
+                                      @ModelAttribute("signingSessionDto") @Valid final SigningSessionDto signingSessionDto,
+                                      BindingResult bindingResult, Authentication authentication, HttpServletRequest request) throws IOException, GeoIp2Exception, GeneralSecurityException {
+
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        User user = userDetails.getUser();
+
+        if (user == null) {
+            System.out.println("User not found");
+        }
+
+        assert user != null;
+        boolean codeVerified = totpService.verifyCode(signingSessionDto.getId(), signingSessionDto.getOtcCode());
+
+        if (!codeVerified) {
+            bindingResult.rejectValue("otcCode", "invalidOTC");
+        }
+
+        if (bindingResult.hasErrors()) {
+            modelAndView.setViewName("website/signing/verifySigning");
+        } else {
+
+            Optional<SigningSession> signingSession = signingSessionService.findById(signingSessionDto.getId());
+            if (signingSession.isPresent()) {
+                SigningSession session = signingSession.get();
+
+                String filepathToLoad = session.getFilepath();
+                Path src = storageService.load(filepathToLoad);
+
+                String clientIp = requestService.getClientIp(request);
+                GeoIP geoIP = locationService.getLocation("87.116.160.153");
+                String location = geoIP.getCity();
+
+                HashCode hash = Files.hash(new File(session.getFilepath()), Hashing.md5());
+
+                String reason = "On behalf of " + user.getFirstName() + " " + user.getLastName() + "\n"
+                        + "Using OTC " + session.getOtcCode() + " and timestamp " + session.getTimestamp() + "\n" +
+                        "Hash value of document: " + hash;
+
+                SigningService signingService = new SigningService();
+                String dest = signingService.sign(src, reason, location);
+                ///////////////////////////////////////////////////
+                modelAndView.addObject("confirmationMessage", user.getFirstName() + ", AES has successfully signed document " + src.getFileName().toString() + " on your behalf!");
+                modelAndView.addObject("documentPath", dest);
+                modelAndView.setViewName("website/signing/signed");
+            }
+
+        }
+        return modelAndView;
+    }
 }
